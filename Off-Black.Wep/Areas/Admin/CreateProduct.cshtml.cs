2e599583a40114066087ce8c4fedb5930b0e8a6f
Peter Hymøller <pete102c@elevcampus.dk>
2021-10-22 09:47:18 +0200

    added more to admin

--- Off-Black.Wep/Areas/Admin/CreateProduct.cshtml.cs
+++ Off-Black.Wep/Areas/Admin/CreateProduct.cshtml.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Off_Black.Services.DTO;
+using Off_Black.Services.Interfaces;
 
 namespace Off_Black.Wep.Areas.Admin
 {
     public class CreateProductModel : PageModel
     {
+        private readonly ILogger<CreateProductModel> _logger;
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public CreateProductModel(ILogger<CreateProductModel> logger, IProductService productService, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        [BindProperty (SupportsGet = true)]
+        public ProductDTO ProductDTOs { get; set; }
+
+
+
         public void OnGet()
         {
         }
--- Off-Black.Wep/Areas/Admin/UpdateProduct.cshtml.cs
+++ Off-Black.Wep/Areas/Admin/UpdateProduct.cshtml.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Off_Black.Services.DTO;
+using Off_Black.Services.Interfaces;
 
 namespace Off_Black.Wep.Areas.Admin
 {
     public class UpdateProductModel : PageModel
     {
+        private readonly ILogger<UpdateProductModel> _logger;
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public UpdateProductModel(ILogger<UpdateProductModel> logger, IProductService productService, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public ProductDTO ProductDTOs { get; set; }
         public void OnGet()
         {
         }
